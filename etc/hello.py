64cbf1bf1968d23dc336587e399005dcccd76f05
Hnatiuk Stanislav <stanislavmain@gmail.com>
2018-07-08 21:14:52 +0300

    fixed

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,2 @@
-import multiprocessing
-
 bind = '0.0.0.0:8080'
 workers = 2
--- init.sh
+++ init.sh
@@ -2,8 +2,16 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
+sudo /etc/init.d/mysql start
+mysql -uroot -e "CREATE USER 'admin'@'localhost'"
+mysql -uroot -e "SET PASSWORD FOR 'admin'@'localhost' = PASSWORD('pass111')"
+mysql -uroot -e "CREATE DATABASE mybase"
+mysql -uroot -e "GRANT ALL ON mybase.* TO 'admin'@'localhost'"
+
 cd /home/box/web/ask
+
+python manage.py makemigrations
+python manage.py migrate
+
 gunicorn -c /home/box/web/etc/ask.py ask.wsgi
-cd /home/box/web
-gunicorn -c /home/box/web/etc/hello.py hello:app
 
